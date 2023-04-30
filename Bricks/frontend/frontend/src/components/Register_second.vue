e6385fc804691f8c73622195856a0c254b163cf2
gary300500 <gary91051916@gmail.com>
2023-04-30 19:17:11 +0800

    register_second

--- Bricks/frontend/frontend/src/components/Register_second.vue
+++ Bricks/frontend/frontend/src/components/Register_second.vue
@@ -16,12 +16,52 @@
                 <div class="select">
                     <div class="card" style="margin-top: 0px;">
                         <p>一、使用 BRICKS 的用途 (多選)</p>
+                        <div class="multiple_select">
+                            <input type="checkbox" id="one_one" name="one">
+                            <label for="one_one" style="margin-left: 0px;">課堂作業</label>
+                            <input type="checkbox" id="one_two" name="one">
+                            <label for="one_two">營隊</label>
+                            <input type="checkbox" id="one_three" name="one">
+                            <label for="one_three">社團</label>
+                            <input type="checkbox" id="one_four" name="one">
+                            <label for="one_four" style="margin-left: 0px;">個人筆記</label>
+                            <input type="checkbox" id="one_five" name="one">
+                            <label for="one_five">競賽</label>
+                            <input type="checkbox" id="one_six" name="one">
+                            <label for="one_six">其它</label>
+                        </div>
                     </div>
                     <div class="card">
                         <p>二、您的身分 (單選)</p>
+                        <div class="multiple_select">
+                            <input type="radio" id="two_one" name="two">
+                            <label for="two_one" style="margin-left: 0px; width: 276px;" >高中職 (含) 以下</label>
+                            <input type="radio" id="two_two" name="two">
+                            <label for="two_two" style="margin-left: 20.5px; width: 276px;">大專院校</label>
+                            <input type="radio" id="two_three" name="two">
+                            <label for="two_three" style="margin-left: 0px;">碩士</label>
+                            <input type="radio" id="two_four" name="two">
+                            <label for="two_four">博士</label>
+                            <input type="radio" id="two_five" name="two">
+                            <label for="two_five">社會人士</label>
+                        </div>
                     </div>
                     <div class="card">
                         <p>三、是否用過其餘專案軟體工具 (多選)</p>
+                        <div class="multiple_select">
+                            <input type="checkbox" id="three_one" name="three">
+                            <label for="three_one" style="margin-left: 0px;">Google 雲端</label>
+                            <input type="checkbox" id="three_two" name="three">
+                            <label for="three_two">Notion</label>
+                            <input type="checkbox" id="three_three" name="three">
+                            <label for="three_three">Trello</label>
+                            <input type="checkbox" id="three_four" name="three">
+                            <label for="three_four" style="margin-left: 0px;">Evernote</label>
+                            <input type="checkbox" id="three_five" name="three">
+                            <label for="three_five">Asana</label>
+                            <input type="checkbox" id="three_six" name="three">
+                            <label for="three_six">其它</label>
+                        </div>
                     </div>
                 </div>
                 <div class="next">
@@ -121,8 +161,7 @@ export default{
     background-image: url(../assets/bricks_bg.svg);
 }
 
-@media screen and (min-width: 1920px) {
-    .middle {
+.middle {
         width: 576px;
         height: 739px;
         position: absolute;
@@ -131,6 +170,7 @@ export default{
         transform: translate(-50%);
     }
 
+@media screen and (min-width: 1920px) {
     .bg {
         height: 950px;
     }
@@ -148,16 +188,8 @@ export default{
     }
 }
 
-@media screen and (min-width: 1600px) and (max-width: 1920px) {
-    .middle {
-        width: 470px;
-        height: 739px;
-        position: absolute;
-        top: 105px;
-        left: 50%;
-        transform: translate(-50%);
-    }
 
+@media screen and (min-width: 1600px) and (max-width: 1920px) {
     .bg {
         height: 950px;
     }
@@ -176,15 +208,6 @@ export default{
 }
 
 @media screen and (max-width: 1600px) {
-    .middle {
-        width: 416px;
-        height: 739px;
-        position: absolute;
-        top: 105px;
-        left: 50%;
-        transform: translate(-50%);
-    }
-
     .bg {
         height: 770px;
     }
@@ -224,7 +247,6 @@ export default{
     width: 100%;
     height: 153px;
     margin-top: 32px;
-    background-color: rosybrown;
 }
 
 .card>p{
@@ -236,6 +258,84 @@ export default{
     position: relative;
     top: 8px;
 }
+
+.multiple_select{
+    width: 100%;
+    height: 90px;
+    position: relative;
+    top: 32px;
+}
+
+.card input[type="checkbox"]{
+    display: none;
+}
+
+.card input[type="radio"]{
+    display: none;
+}
+
+.card input[type="checkbox"] + label{
+    display: inline-block;
+    background-color: white;
+    cursor: pointer;
+    width: 174px;
+    height: 39px;
+    border: 1px solid #b6aeae;
+    border-radius: 12px;
+    color: #3b3838;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: 1.25px;
+    line-height: 39px;
+    text-align: center;
+    user-select: none;
+    margin-left: 24.5px;
+    margin-bottom: 10px;
+}
+
+.card input[type="radio"] + label{
+    display: inline-block;
+    background-color: white;
+    cursor: pointer;
+    width: 174px;
+    height: 39px;
+    border: 1px solid #b6aeae;
+    border-radius: 12px;
+    color: #3b3838;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: 1.25px;
+    line-height: 39px;
+    text-align: center;
+    user-select: none;
+    margin-left: 24.5px;
+    margin-bottom: 10px;
+}
+
+.card input[type="checkbox"]:hover + label{
+    background-color: #f2eeee;
+}
+
+.card input[type="radio"]:hover + label{
+    background-color: #f2eeee;
+}
+
+.card input[type="checkbox"]:active + label{
+    background-color: #f1d5d6;
+}
+
+.card input[type="radio"]:active + label{
+    background-color: #f1d5d6;
+}
+
+.card input[type="checkbox"]:checked + label{
+    background-color: #f1d5d6;
+}
+
+.card input[type="radio"]:checked + label{
+    background-color: #f1d5d6;
+}
+
 
 .next{
     width: 100%;
@@ -263,10 +363,26 @@ export default{
     text-decoration: none;
 }
 
+.skip div:hover{
+    background-color: #f2eeee;
+}
+
+.skip div:active{
+    background-color: #f1d5d6;
+}
+
 .complete{
     float: right;
     border-color: #b82c30;
     text-decoration: none;
+}
+
+.complete div:hover{
+    background-color: #d48083 !important;
+}
+
+.complete div:active{
+    background-color: #932326 !important;
 }
 
 .bottom {
