a66657c8b124278585fcbd6ce139a89a64f6c6de
gary300500 <gary91051916@gmail.com>
2023-05-17 00:28:18 +0800

    personal_homepage

--- Bricks/frontend/frontend/src/router/index.js
+++ Bricks/frontend/frontend/src/router/index.js
@@ -8,7 +8,8 @@ import Login_2 from "../components/Login_2.vue";
 import Register_2 from "../components/Register_2.vue";
 import Homepage_2 from "../components/Homepage_2.vue";
 import Register_second from "../components/Register_second.vue";
-import Personal_homepage from "../components/Personal_homepage.vue"
+import Personal_homepage from "../components/Personal_homepage.vue";
+import test from "../components/test.vue";
 
 Vue.use(VueRouter);
 
@@ -59,6 +60,11 @@ const routes = [
     path: "/personal_homepage",
     name: "Personal_homepage",
     component: Personal_homepage,
+    },
+  {
+    path: "/test",
+    name: "test",
+    component: test,
     },
 ];
 
